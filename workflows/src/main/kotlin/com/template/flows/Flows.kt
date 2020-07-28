83b996205f8f405f94243478d82a78a709785fd6
Peter de Rooij <peter.de.rooij@accenture.com>
2020-07-28 17:38:21 +0100

    Initial Commit

--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -3,25 +3,36 @@ package com.template.flows
 import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.flows.*
 import net.corda.core.utilities.ProgressTracker
+import net.corda.core.utilities.unwrap
 
 // *********
 // * Flows *
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
+class EchoInitiator(val message: String, val counterParty: String) : FlowLogic<String>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
-    override fun call() {
-        // Initiator flow logic goes here.
+    override fun call(): String {
+        val party = serviceHub.identityService.partiesFromName(counterParty, false).single()
+        val counterPartySession = initiateFlow(party)
+        counterPartySession.send(message)
+        System.out.println("sent message: \"$message\" to ${party.name}")
+        val reverse = counterPartySession.receive<String>().unwrap { it }
+        System.out.println("received reverse echo: \"$reverse\"")
+        return reverse
     }
 }
 
-@InitiatedBy(Initiator::class)
-class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+@InitiatedBy(EchoInitiator::class)
+class EchoResponder(val counterPartySession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
-        // Responder flow logic goes here.
+        val message = counterPartySession.receive<String>().unwrap { it }
+        System.out.println("received message \"$message\" from ${counterPartySession.counterparty.name}")
+        val reverse = message.reversed()
+        counterPartySession.send(reverse)
+        System.out.println("returned reverse \"$reverse\"")
     }
 }
